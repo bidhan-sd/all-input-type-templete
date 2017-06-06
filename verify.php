360c33c2c63a37e5d82e5905b26e6d3843825ba0
Bidhan sutradhar <bidhanvk@gmail.com>
2017-06-06 14:35:26 +0600

    verify page done here

--- verify.php
+++ verify.php
@@ -1,19 +1,35 @@
 <?php 
+
 	if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['token']) && !empty($_GET['token'])){
+	    session_start();
 	    // Verify data
+	    mysql_connect("localhost", "dreambdn_bUsr", "vxFF%Izl63^#") or die(mysql_error());
+	    mysql_select_db("dreambdn_bDB") or die(mysql_error());
+	    
+	    $email     = filter_var($_GET['email'], FILTER_SANITIZE_STRING);
+	    $token     = filter_var($_GET['token'], FILTER_SANITIZE_STRING);
+	    
 	    $email = mysql_escape_string($_GET['email']);
 	    $token = mysql_escape_string($_GET['token']);
-	    mysql_connect("localhost", "root", "") or die(mysql_error());
-	    mysql_select_db("curd") or die(mysql_error());
-	    $search = mysql_query("SELECT email, token, active FROM users WHERE email='".$email."' AND token='".$token."' AND active='0'") or die(mysql_error()); 
-		$match  = mysql_num_rows($search);
-		if($match > 0){
-	    	mysql_query("UPDATE users SET active='1' WHERE email='".$email."' AND token='".$token."' AND active='0'") or die(mysql_error());
-			$success = " <span style='color:green;font-weight:bold'>Your account has been activated, you can now login</span> ";
-		}else{
-		    $error = "<span style='color:red;font-weight:bold'>The url is either invalid or you already have activated your account.</span> ";
-		}
-
+	    
+        $sql =  "SELECT email, token, active FROM users WHERE email='".$email."' AND token='".$token."'";
+	    $search = mysql_query($sql) or die(mysql_error());
+	    $result = mysql_fetch_assoc($search);
+	    if($result['email'] === $email && $result['token'] === $token){
+    	    if($result['email'] === $email && $result['token'] === $token && $result['active'] == 1){
+    			$_SESSION['message'] = "<span style='color:red;font-weight:bold'>Account already activated!</span> ";
+    		    header("location:signin.php");
+    		}else{
+    		    if($result['email'] === $email && $result['token'] === $token && $result['active'] == 0){
+        		    mysql_query("UPDATE users SET active='1' WHERE email='".$email."' AND token='".$token."' AND active='0'") or die(mysql_error());
+        			$_SESSION['message'] = "<span style='color:green;font-weight:bold'> Account activated !</span> ";
+        			header("location:signin.php");
+    		    }
+    		}
+	    }else{
+    		$_SESSION['message'] = "<span style='color:red;font-weight:bold'>You given invlaid url!</span> ";
+    	    header("location:signin.php");
+	    }
 	}else{
 		session_start();
 		header("Location: signup.php");
