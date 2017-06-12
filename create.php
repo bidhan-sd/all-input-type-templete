aeb866a6da768a28ac594670e3c9ec503cc8953e
Bidhan sutradhar <bidhanvk@gmail.com>
2017-06-12 20:46:17 +0600

    student added successfully done here

--- create.php
+++ create.php
@@ -1,10 +1,10 @@
 
 <?php include 'inc/header.php'; ?>
 <?php
-	/*session_start();
+	session_start();
 	if(empty($_SESSION['userinfo'])){
         header ("Location: signin.php");
-	}*/
+	}
 ?>
 <?php
     if(isset($_POST['submit'])){
@@ -25,43 +25,54 @@
         		if(!filter_var($website, FILTER_VALIDATE_URL)){
 					$error = "<span style='color:red;font-weight:bold'>Website address Invalid.</span>";
 	        	}else{
+
 	        		$allowed      =  array('gif','png' ,'jpg');
 					$filename     = $_FILES['image']['name'];
 					$templocation = $_FILES['image']['tmp_name'];
 					$filesize     = $_FILES['image']['size'];
-					$filekb = ceil($filesize/1024);
+					$filekb       = ceil($filesize/1024);
                     
                     
-					$ext = pathinfo($filename, PATHINFO_EXTENSION);
-                    //$info = explode('.', strtolower($filename); // whats the extension of the file
-                    $fileformate = strtolower($ext);
-                    
-                    //For more details : https://stackoverflow.com/questions/13470482/php-upload-image-to-directory
-                    
-					if(!in_array($fileformate,$allowed) ) {
+                    /*
+	                    $info = explode('.',$_FILES['image']['name']);
+					    $file_ext = strtolower(end($info));
+					    $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+					    $uploaded_image = "upload/".$unique_image;
+				    */
+
+					$ext            = pathinfo($filename, PATHINFO_EXTENSION);//Find out File extension
+                    $fileformate    = strtolower($ext);
+                    $unique_image   = substr(md5(time()), 0, 10).'.'.$fileformate;
+				    $uploaded_image = "uploads/".$unique_image;
+				    
+                    if(!in_array($fileformate,$allowed) ) {
 					 	$error = "<span style='color:red;font-weight:bold'>Image format must be jpg,png,gif,jpeg.</span>";
 					}else{
 						if($filekb > 1048576){//1048576 == 2mb
 							$error = "<span style='color:red;font-weight:bold'>File size must be content within 2 mb.</span>";
 						}else{
-								$link = mysql_connect("localhost", "root", "");
-								mysql_select_db("crud", $link);
 
-								$name     = mysql_real_escape_string($name, $link);
-								$email    = mysql_real_escape_string($email, $link);
-								$website  = mysql_real_escape_string($website, $link);
-								$country  = $_POST['country'];
-                                $subjects = implode(",",$_POST['subject']);
-								$gender   = $_POST['gender'];
-                                $uniqId   = "adfdf45215963587";
-                            
-								$sql = "INSERT INTO addstudent (id,userId,name,email,website,country,subject,gender,image,created_at,updated_at,deleted_at) VALUES (null,'$uniqId','$name','$email','$website','$country','$subjects','$gender','$filename','','','')";
-								$result = mysql_query($sql);
-								if($result){
-									$error = "<span style='color:green;font-weight:bold'>Student Added Successflly.</span>";
-								}else{
-									$error = "<span style='color:red;font-weight:bold'>Insertion Failed.</span>";
-								}
+							$link = mysql_connect("localhost", "root", "");
+							mysql_select_db("crud", $link);
+
+							$name     = mysql_real_escape_string($name, $link);
+							$email    = mysql_real_escape_string($email, $link);
+							$website  = mysql_real_escape_string($website, $link);
+							$country  = $_POST['country'];
+                            $subjects = implode(",",$_POST['subject']);
+							$gender   = $_POST['gender'];
+                            $userId   = $_SESSION['userinfo']['userId'];
+                            $date     = date('Y-m-d');
+
+                            move_uploaded_file($templocation, $uploaded_image);
+							$sql = "INSERT INTO addstudent (id,userId,name,email,website,country,subject,gender,image,created_at,updated_at,deleted_at) VALUES (null,'$userId','$name','$email','$website','$country','$subjects','$gender','$unique_image','$date','','')";
+
+							$result = mysql_query($sql);
+							if($result){
+								$error = "<span style='color:green;font-weight:bold'>Student Added Successflly.</span>";
+							}else{
+								$error = "<span style='color:red;font-weight:bold'>Insertion Failed.</span>";
+							}
 						}
 					}
 	        	}
