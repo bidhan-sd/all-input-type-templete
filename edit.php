b56f3ffb3693543b976a9b4c605329381db4785b
Bidhan sutradhar <bidhanvk@gmail.com>
2017-06-16 23:52:25 +0600

    edit page modify here but work continuously running

--- edit.php
+++ edit.php
@@ -8,7 +8,73 @@
     if(isset($_GET['id']) && isset($_GET['userId'])){
         $id = $_GET['id'];
         $userId = $_GET['userId'];
-    }
+    }    
+?>
+<?php
+    if(isset($_POST['submit'])){
+        $error = '';
+        $success = '';
+
+        if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['website']) || empty($_POST['country']) || empty($_POST['subject']) || empty($_POST['gender']) || empty($_FILES['image']) ){
+			
+			$error = "<span style='color:red;font-weight:bold'>Require field can't be empty...</span>";
+        }else{
+        	$name     = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        	$email    = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        	$website  = filter_var($_POST['website'], FILTER_SANITIZE_URL);
+
+        	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				$error = "<span style='color:red;font-weight:bold'>Email address Invalid.</span>";
+        	}else{
+        		if(!filter_var($website, FILTER_VALIDATE_URL)){
+					$error = "<span style='color:red;font-weight:bold'>Website address Invalid.</span>";
+	        	}else{
+
+	        		$allowed      =  array('gif','png' ,'jpg');
+					$filename     = $_FILES['image']['name'];
+					$templocation = $_FILES['image']['tmp_name'];
+					$filesize     = $_FILES['image']['size'];
+					$filekb       = ceil($filesize/1024);
+
+					$ext            = pathinfo($filename, PATHINFO_EXTENSION);//Find out File extension
+                    $fileformate    = strtolower($ext);
+                    $unique_image   = substr(md5(time()), 0, 10).'.'.$fileformate;
+				    $uploaded_image = "uploads/".$unique_image;
+				    
+                    if(!in_array($fileformate,$allowed) ) {
+					 	$error = "<span style='color:red;font-weight:bold'>Image format must be jpg,png,gif,jpeg.</span>";
+					}else{
+						if($filekb > 1048576){//1048576 == 2mb
+							$error = "<span style='color:red;font-weight:bold'>File size must be content within 2 mb.</span>";
+						}else{
+
+							$link = mysql_connect("localhost", "root", "");
+							mysql_select_db("crud", $link);
+
+							$name     = mysql_real_escape_string($name, $link);
+							$email    = mysql_real_escape_string($email, $link);
+							$website  = mysql_real_escape_string($website, $link);
+							$country  = $_POST['country'];
+                            $subjects = implode(",",$_POST['subject']);
+							$gender   = $_POST['gender'];
+                            $userId   = $_SESSION['userinfo']['userId'];
+                            $date     = date('Y-m-d');
+
+                            move_uploaded_file($templocation, $uploaded_image);
+							$sql = "INSERT INTO addstudent (id,userId,name,email,website,country,subject,gender,image,created_at,updated_at,deleted_at) VALUES (null,'$userId','$name','$email','$website','$country','$subjects','$gender','$uploaded_image','$date','','')";
+
+							$result = mysql_query($sql);
+							if($result){
+								$error = "<span style='color:green;font-weight:bold'>Student Added Successflly.</span>";
+							}else{
+								$error = "<span style='color:red;font-weight:bold'>Insertion Failed.</span>";
+							}
+						}
+					}
+	        	}
+        	}
+        }
+	}
 ?>
 <?php include 'inc/header.php'; ?>
 
@@ -18,10 +84,6 @@
     $sql = "SELECT * FROM addstudent WHERE id='$id' && userId='$userId'";
     $query = mysql_query($sql, $link);
     $result =  mysql_fetch_assoc($query);
-    
-
-
-
 ?>
 
 
